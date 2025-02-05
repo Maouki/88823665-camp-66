11d45028f6aaac8e555b988b0f9003cc9cdde96e
Chanwit <iopbnm0874073933@gmail.com>
2025-02-05 14:55:44 +0700

    edit,delete (Users)

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\MyController;
 use App\Http\Controllers\loginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/login',
     [LoginController::class, 'login']);
 
 Route::get('/register',
-    [RegisterController::class, 'register']);
+    [RegisterController::class, 'regis']);
 
 Route::get('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
@@ -18,12 +19,24 @@ Route::get('/mycontroller/{id?}',
 Route::post('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
 
+    Route::post('/register',
+    [RegisterController::class, 'create']);
+
+Route::get('/users' , [UserController::class , 'index']);
+
+Route::get('/user/{id}' , [UserController::class , 'edit']);
+
+Route::put('/user' , [UserController::class , 'edit_action']);
+
+Route::delete('/user' , [UserController::class , 'delete']);
+
+
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/home',
-    [HomeController::class, 'home']);
+    [HomeController::class, 'House']);
 
 
 Route::get('/hello{/id?}',
