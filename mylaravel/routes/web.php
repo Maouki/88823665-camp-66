c481a3b4a9da9c526d04ab5c47584637188b0272
Chanwit <iopbnm0874073933@gmail.com>
2025-02-26 21:39:35 +0700

    Login และ Dynamic Form

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,9 +6,37 @@ use App\Http\Controllers\loginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckLogin;
+use App\Http\Controllers\ProductController;
+
+
+Route::middleware([CheckLogin::class])->group(function () {
+    Route::get('/users' , [UserController::class , 'index']);
+    Route::get('/user/{id}' , [UserController::class , 'edit']);
+    Route::put('/user' , [UserController::class , 'edit_action']);
+    Route::delete('/user' , [UserController::class , 'delete']);
+
+    Route::get('/product', [ProductController::class, 'index']);
+    Route::post('/product', [ProductController::class, 'add_product']);
+});
+
+Route::get('/',
+[HomeController::class , 'index'])->middleware([CheckLogin::class]);
+
+
 
 Route::get('/login',
+    [LoginController::class, 'index']);
+
+Route::post('/login',
     [LoginController::class, 'login']);
+
+Route::get('logout',function(){
+session()->forget('user');
+session()->flush();
+return redirect('/login');
+});
+
 
 Route::get('/register',
     [RegisterController::class, 'regis']);
@@ -30,14 +58,6 @@ Route::post('/mycontroller/{id?}',
     [MyController::class, 'myfunction']);
 
 
-
-Route::get('/users' , [UserController::class , 'index']);
-
-Route::get('/user/{id}' , [UserController::class , 'edit']);
-
-Route::put('/user' , [UserController::class , 'edit_action']);
-
-Route::delete('/user' , [UserController::class , 'delete']);
 
 
 Route::get('/', function () {
